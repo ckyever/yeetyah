b2a50727d17c4329b9110101a55b03f53216455d
Ckyever Gaviola <ckyeverg@gmail.com>
2026-01-07 17:29:07 +1100

    Generate a jsonwebtoken and return it upon creating a new user

--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcryptjs";
 import type { Request, Response } from "express";
 import { constants as httpConstants } from "http2";
+import jwt from "jsonwebtoken";
 
 import * as usersModel from "../models/usersModel";
 
@@ -12,7 +13,12 @@ interface UserParams {
 }
 
 const createUser = async (req: Request<UserParams>, res: Response) => {
-  const { username, password, display_name, profile_image } = req.body;
+  const {
+    username,
+    password,
+    display_name: displayName,
+    profile_image: profileImage,
+  } = req.body;
 
   if (!username || !password) {
     return res
@@ -26,19 +32,38 @@ const createUser = async (req: Request<UserParams>, res: Response) => {
   const newUser = await usersModel.createUser(
     username,
     hashedPassword,
-    display_name,
-    profile_image
+    displayName,
+    profileImage
   );
 
-  if (newUser) {
-    return res
-      .status(httpConstants.HTTP_STATUS_CREATED)
-      .json({ username, display_name, profile_image });
-  } else {
+  if (!newUser) {
     return res
       .status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
       .json({ message: "Failed to create a user" });
   }
+
+  if (!process.env.SECRET_KEY) {
+    throw new Error("Missing secret key environment variable");
+  }
+
+  const secret: string = process.env.SECRET_KEY;
+  jwt.sign(
+    { user: newUser },
+    secret,
+    { expiresIn: "1 days" },
+    (error, token) => {
+      if (error) {
+        console.error(error);
+        return res
+          .status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
+          .json({ message: "Failed to get a token" });
+      } else {
+        return res
+          .status(httpConstants.HTTP_STATUS_CREATED)
+          .json({ user_id: newUser.id, token });
+      }
+    }
+  );
 };
 
 export { createUser };
