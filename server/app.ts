08f9f87dde0445064c94257934d3011160c2cfea
Ckyever Gaviola <ckyeverg@gmail.com>
2026-01-07 12:44:22 +1100

    Add comments to outline API endpoints plan

--- server/app.ts
+++ server/app.ts
@@ -6,6 +6,18 @@ app.get("/", (req, res) => res.send("Hello there"));
 
 const PORT = process.env.PORT || 3000;
 
+// Login a user - POST /api/auth/login
+// Logout a user - POST /api/auth/logout
+
+// Get users - GET /api/users
+// Create a user - POST /api/users
+// Get user profile - GET /api/users/:userId
+// Update profile - PUT /api/users/:userId
+
+// Get chat list - GET /api/chats
+// View messages in chat - GET /api/chats/:chatId/messages
+// Send message - POST /api/chats/:chatId/messages
+
 app.listen(PORT, (error) => {
   if (error) {
     throw error;
