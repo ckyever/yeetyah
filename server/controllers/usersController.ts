50cbcf449ae9ca0eeddc2e430f1d8c2c368a082b
Ckyever Gaviola <ckyeverg@gmail.com>
2026-01-08 11:21:09 +1100

    Add validation to body form data when creating a user

--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -1,8 +1,10 @@
 import { hash } from "bcryptjs";
-import type { Request, Response } from "express";
+import type { Request, Response, NextFunction } from "express";
+import * as validator from "express-validator";
 import { constants as httpConstants } from "http2";
 import jwt from "jsonwebtoken";
 
+import { VALIDATION } from "../lib/constants";
 import * as usersModel from "../models/usersModel";
 
 interface UserParams {
@@ -12,58 +14,84 @@ interface UserParams {
   profile_image: string;
 }
 
-const createUser = async (req: Request<UserParams>, res: Response) => {
-  const {
-    username,
-    password,
-    display_name: displayName,
-    profile_image: profileImage,
-  } = req.body;
+const validateUser = [
+  validator
+    .body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ max: VALIDATION.USERNAME.MAX_LENGTH })
+    .withMessage(
+      `Username must be ${VALIDATION.USERNAME.MAX_LENGTH} characters or less`
+    ),
+  validator
+    .body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: VALIDATION.PASSWORD.MIN_LENGTH })
+    .withMessage(
+      `Password must be ${VALIDATION.PASSWORD.MIN_LENGTH} characters or more`
+    ),
+  validator.body("display_name").trim(),
+  validator.body("profile_image").trim(),
+];
 
-  if (!username || !password) {
-    return res
-      .status(httpConstants.HTTP_STATUS_BAD_REQUEST)
-      .json({ message: "Invalid create user parameters" });
-  }
-
-  const SALT_ROUNDS = 10;
-  const hashedPassword = await hash(password, SALT_ROUNDS);
-
-  const newUser = await usersModel.createUser(
-    username,
-    hashedPassword,
-    displayName,
-    profileImage
-  );
-
-  if (!newUser) {
-    return res
-      .status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
-      .json({ message: "Failed to create a user" });
-  }
-
-  if (!process.env.SECRET_KEY) {
-    throw new Error("Missing secret key environment variable");
-  }
-
-  const secret: string = process.env.SECRET_KEY;
-  jwt.sign(
-    { user: newUser },
-    secret,
-    { expiresIn: "1 days" },
-    (error, token) => {
-      if (error) {
-        console.error(error);
-        return res
-          .status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
-          .json({ message: "Failed to get a token" });
-      } else {
-        return res
-          .status(httpConstants.HTTP_STATUS_CREATED)
-          .json({ user_id: newUser.id, token });
-      }
+const createUser = [
+  validateUser,
+  async (req: Request<UserParams>, res: Response, next: NextFunction) => {
+    const errors = validator.validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(httpConstants.HTTP_STATUS_BAD_REQUEST)
+        .json({ message: "Invalid create user body", errors: errors.array() });
     }
-  );
-};
+
+    const {
+      username,
+      password,
+      display_name: displayName,
+      profile_image: profileImage,
+    } = validator.matchedData(req);
+
+    const SALT_ROUNDS = 10;
+    const hashedPassword = await hash(password, SALT_ROUNDS);
+
+    const newUser = await usersModel.createUser(
+      username,
+      hashedPassword,
+      displayName,
+      profileImage
+    );
+
+    if (!newUser) {
+      return res
+        .status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
+        .json({ message: "Failed to create a user" });
+    }
+
+    if (!process.env.SECRET_KEY) {
+      throw new Error("Missing secret key environment variable");
+    }
+
+    const secret: string = process.env.SECRET_KEY;
+    jwt.sign(
+      { user: newUser },
+      secret,
+      { expiresIn: "1 days" },
+      (error, token) => {
+        if (error) {
+          console.error(error);
+          return res
+            .status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR)
+            .json({ message: "Failed to get a token" });
+        } else {
+          return res
+            .status(httpConstants.HTTP_STATUS_CREATED)
+            .json({ user_id: newUser.id, token });
+        }
+      }
+    );
+  },
+];
 
 export { createUser };
---
+++ server/lib/constants.ts
@@ -0,0 +1,8 @@
+export const VALIDATION = {
+  USERNAME: {
+    MAX_LENGTH: 30,
+  },
+  PASSWORD: {
+    MIN_LENGTH: 8,
+  },
+};
